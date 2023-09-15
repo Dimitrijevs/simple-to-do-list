521299fea00941af36f4a9242d5a10db24454e96
Dimitrijevs <githubodinlv@gmail.com>
2023-09-15 17:53:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,25 @@ const toDo = document.querySelector('#newtask input');
 
 function addToDo(){
 
-    task.innerHTML += `
-        <div class="task">
-            <span id="taskname">
-                ${toDo.value}
-            </span>
-            <button class="delete">Delete</button>
-        </div>
-    `;
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please enter task")
+    } else {
+        task.innerHTML += `
+            <div class="task-container">
+                <span class="task-text">
+                    ${toDo.value}
+                </span>
+                <button class="delete">Delete</button>
+            </div>
+        `;
 
-    toDo.value = "";
+        toDo.value = "";
 
-    let current_tasks = document.querySelectorAll(".delete");
-        for(let i=0; i<current_tasks.length; i++){
-        current_tasks[i].onclick = function(){
-            this.parentNode.remove();
+        let current_tasks = document.querySelectorAll(".delete");
+            for(let i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
         }
     }
 }
